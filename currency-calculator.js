1263ff42c644963b8d835f6d54d6d8f24f1463ab
Agamka <agnieszkaasowinska@gmail.com>
2021-11-01 22:08:45 +0100

    added functions: showCurrencies, showExchangeRatesList

--- currency-calculator.js
+++ currency-calculator.js
@@ -1,73 +1,90 @@
-document.querySelector('#tester button').addEventListener('click', function() {
-    var ajax = new XMLHttpRequest();
+ let requestURL = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+ let method = "GET";
+ let ajax = new XMLHttpRequest();
+ ajax.open(method, requestURL , true);
+ ajax.send();
+ //function loadDoc() {
     ajax.onreadystatechange = function() {
-      var res, kursy;
+      var res;
       if(ajax.readyState === 4 && ajax.status === 200) {
         res = JSON.parse(this.responseText);
-        //kursy = res[0].rates[0].currency+': '+res[0].rates[0].mid;
+        showExchangeRatesList(res)
+        showCurrencies(res);
+      }
+    };
+ // };
+  //ajax.send();
+
+  //document.querySelector('#tester button').addEventListener('click', loadDoc);
+
+  function showExchangeRatesList(res){
         let x = "";
         let y = "";
         let z = "";
           for (i in res[0].rates) 
             {
-              x += res[0].rates[i].currency+ " " + res[0].rates[i].code+ " " + res[0].rates[i].mid+ "</br>";
-              console.log(x);
+              x += "1 " + res[0].rates[i].code
+              + " (" + res[0].rates[i].currency+ ")"
+              + " = " 
+              + res[0].rates[i].mid
+              + " PLN"
+              + "</br>";
+              y += res[0].rates[i].code;
             }
-        //kursy = res.rates[0].effectiveDate+': '+res.rates[0].mid;
         document.querySelector('#tester p').innerHTML = x;
-      }
-    };
-    ajax.open("GET", "http://api.nbp.pl/api/exchangerates/tables/a/?format=json", true);
-    //ajax.open("GET", "http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json", true);
-    ajax.send();
-  });
-  //ajax.send();
+  }
 
-var rates = [0.25, 0.22, 3.98, 0.86, 4.62, 1.16];
+  function showCurrencies(res){
+    var select = document.getElementById("currency");
+    var select2 = document.getElementById("currency-2");
+    for (i = 0; i<res[0].rates.length; i++){
+      var optn = res[0].rates[i].code 
+      var val = res[0].rates[i].mid
+      //+ " " + res[0].rates[i].mid;
+      var el = document.createElement("option");
+      el.textContent = optn;
+      el.value = val;
+      select.appendChild(el);
+    }
+   /* for (i = 0; i<res[0].rates.length; i++){
+      var optn = res[0].rates[i].code;
+      var el = document.createElement("option");
+      el.textContent = optn;
+      el.value = optn;
+      select2.appendChild(el);
+    }*/
+  }
+  
 
-var PLNUSD = rates[0];
-var PLNEUR = rates[1];
-var USDPLN = rates[2];
-var USDEUR = rates[3];
-var EURPLN = rates[4];
-var EURUSD = rates[5];
+  var btn = document.getElementById('btn');
+  var input = document.getElementById('amount');
+  var currency1 = document.getElementById('currency');
+  var currency2 = document.getElementById('currency-2');
+  var showNumber = document.getElementById('number');
+  var showCurrency = document.getElementById('curr');
+  var showResult = document.getElementById('result');
+  var showCurrency2 = document.getElementById('curr-2');
 
-var btn = document.getElementById('btn');
-var input = document.getElementById('amount');
-var currency1 = document.getElementById('currency');
-var currency2 = document.getElementById('currency-2');
-
-var showNumber = document.getElementById('number');
-var showCurrency = document.getElementById('curr');
-var showResult = document.getElementById('result');
-var showCurrency2 = document.getElementById('curr-2');
-
-function calculate(event){
+  function calculate(event, res){
     event.preventDefault();
     var amount = input.value;
     var from = currency1.value;
+    var fromCurr = currency1.text;
     var to = currency2.value;
     var result = 0;
-   
-    if(from === 'PLN' && to === 'USD'){
-       result = amount * PLNUSD;
-    } else if (from === 'USD' && to === 'PLN') {
-        result = amount * USDPLN;
-      } else if (from === 'PLN' && to === 'EUR') {
-        result = amount * PLNEUR;
-      } else if (from === 'EUR' && to === 'PLN') {
-        result = amount * EURPLN;
-      } else if (from === 'USD' && to === 'EUR') {
-        result = amount * USDEUR;
-      } else if (from === 'EUR' && to === 'USD') {
-        result = amount * EURUSD;
-      }
+    console.log(from, fromCurr)
+    result = amount *  from;
 
+    /*var x = document.getElementById("currency").options.length;
+    console.log(x)*/
+  
     showNumber.innerHTML = amount;
-    showCurrency.textContent = from;
+    showCurrency.textContent = fromCurr;
     showResult.textContent = ' = ' + result;
     showCurrency2.textContent = to;
-    
-}
+  }
+  
 
-btn.addEventListener('click', calculate)
+  btn.addEventListener('click', calculate)
+
+
